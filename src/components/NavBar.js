a08c451e22ae1068dba2a1e7fafe1d8f4fadb1c4
Aayush241002 <aayushkpal948@gmail.com>
2025-09-02 18:23:44 +0530

    css changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,52 @@
-import React from 'react'
+import React from 'react';
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
-import countries from './countries'; //  Import the country list
+import countries from './countries'; // Country list with code, name, flag
 
 const NavBar = () => {
-
   return (
     <>
       <nav className="navbar fixed-top bg-black navbar-dark shadow-sm py-3">
-        <div className="container">
-          <NavLink className="navbar-brand fw-bold text-uppercase" to="/">News Monkey</NavLink>
-          <div className="navbar-nav ms-auto d-flex flex-wrap justify-content-end align-items-center">
+        <div className="container d-flex flex-column flex-md-row align-items-center justify-content-between flex-wrap">
+          <NavLink className="navbar-brand fw-bold text-uppercase me-3" to="/">News Monkey</NavLink>
 
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="/">general</NavLink>
-              </li>
+          <ul className="navbar-nav d-flex flex-row flex-wrap justify-content-center align-items-center w-100">
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/">general</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/business">business</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/entertainment">entertainment</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/sports">sports</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/health">health</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/science">science</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-underline text-uppercase" to="/technology">technology</NavLink>
+            </li>
+          </ul>
 
-
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="/business">business</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="/entertainment">entertainment</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="/sports">sports</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="health">health</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="science">science</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link nav-underline text-uppercase" to="/technology">technology</NavLink>
-              </li>
-
-
-            </ul>
-            <button
-              className="btn btn-outline-light position-absolute top-0 end-0 m-3"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasRight"
-              aria-controls="offcanvasRight"
-            >
-              ☰
-            </button>
-
-
-          </div>
+          <button
+            className="btn btn-outline-light ms-md-3 mt-3 mt-md-0"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasRight"
+            aria-controls="offcanvasRight"
+          >
+            ☰
+          </button>
         </div>
       </nav>
+
+      {/* Offcanvas menu for extras */}
       <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
         <div className="offcanvas-header">
           <h5 className="offcanvas-title" id="offcanvasRightLabel">More Options</h5>
@@ -74,15 +68,10 @@ const NavBar = () => {
           </ul>
           <NavLink className="nav-link text-white mb-2 my-3" to="/about">About</NavLink>
           <NavLink className="nav-link text-white mb-2" to="/contact">Contact</NavLink>
-          {/* Add more links or content here */}
         </div>
       </div>
-
-
     </>
+  );
+};
 
-  )
-
-}
-
-export default NavBar
+export default NavBar;
