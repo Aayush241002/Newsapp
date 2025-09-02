fe6b936650ed57f2e4174266fbbea21d8a38c4f9
Aayush241002 <aayushkpal948@gmail.com>
2025-09-02 18:50:16 +0530

    final css updation changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,77 +1,64 @@
-import React from 'react';
+import React from 'react'
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
-import countries from './countries'; // Country list with code, name, flag
+import countries from './countries'; //  Import the country list
 
 const NavBar = () => {
+
   return (
     <>
-      <nav className="navbar fixed-top bg-black navbar-dark shadow-sm py-3">
-        <div className="container d-flex flex-column flex-md-row align-items-center justify-content-between flex-wrap">
-          <NavLink className="navbar-brand fw-bold text-uppercase me-3" to="/">News Monkey</NavLink>
-
-          <ul className="navbar-nav d-flex flex-row flex-wrap justify-content-center align-items-center w-100">
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/">general</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/business">business</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/entertainment">entertainment</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/sports">sports</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/health">health</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/science">science</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link nav-underline text-uppercase" to="/technology">technology</NavLink>
-            </li>
-          </ul>
-
-          <button
-            className="btn btn-outline-light ms-md-3 mt-3 mt-md-0"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasRight"
-            aria-controls="offcanvasRight"
-          >
-            ☰
+      <nav className="navbar fixed-top navbar-expand-lg bg-black navbar-dark shadow-sm py-3">
+        <div className="container">
+          <NavLink className="navbar-brand fw-bold text-uppercase" to="/">News Monkey</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span className="navbar-toggler-icon"></span>
           </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/">general</NavLink>
+              </li>
+
+
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/business">business</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/entertainment">entertainment</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/sports">sports</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="health">health</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="science">science</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/technology">technology</NavLink>
+              </li>
+      
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/about">About</NavLink>
+
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link nav-underline text-uppercase" to="/contact">Contact</NavLink>
+
+              </li>
+
+            </ul>
+
+
+          </div>
         </div>
       </nav>
-
-      {/* Offcanvas menu for extras */}
-      <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasRightLabel">More Options</h5>
-          <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-          <a className="nav-link nav-underline text-uppercase dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Country
-          </a>
-          <ul className="dropdown-menu">
-            {countries.map((country) => (
-              <li key={country.code}>
-                <NavLink className="dropdown-item d-flex align-items-center" to={`/country/${country.code}`}>
-                  <span className="me-2">{country.flag}</span>
-                  {country.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-          <NavLink className="nav-link text-white mb-2 my-3" to="/about">About</NavLink>
-          <NavLink className="nav-link text-white mb-2" to="/contact">Contact</NavLink>
-        </div>
-      </div>
     </>
-  );
-};
 
-export default NavBar;
+  )
+
+}
+
+export default NavBar
